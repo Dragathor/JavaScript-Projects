8021aaba9b02d6cbe0530c4b36c6f3de1b3f01a7
Dragathor <lewis.lowrie@gmail.com>
2026-05-25 12:03:04 +0100

    Finished Project10_loops_arrays

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -14,22 +14,85 @@ function length2() {
     //click again for current shown string length so "2"
 }
 
-    var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
-    var Content = "";
-    var Y;
     function for_Loop() {
-        for (Y = 0; Y < Instruments.length; Y++) {
+    var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"]; //arrey 
+    var Content = ""; //set undefined variable
+    var Y; //variable for index no.
+
+        for (Y = 0; Y < Instruments.length; Y++) { //move though inxed and increase inxed if Y isn't = to no. of items in index
         Content += Instruments[Y] + "<br>";
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
 
 
-function cat_pics() {
+
+    var Z = 0;
+ function array_Function() {
     var Cat_Picture = [];
     Cat_Picture[0] = "sleeping";
     Cat_Picture[1] = "playing";
     Cat_Picture[2] = "eating";
     Cat_Picture[3] = "purring";
-    document.getElementById("Cat").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
+    
+    if (Z < 3){
+        Z = Z + 1} //each click of button displays one increase of the array index possition 
+        else {(Z = 0)} // if index denominator would be > than 3 then set to 0
+    
+    document.getElementById("Array").innerHTML = "In this picture, the cat is " + Cat_Picture[Z] + "."; //show each array value as the index is increased with each click
+}
+
+
+function constant_function() {
+    const Musical_Instrument = {type: "guitar", brand:"Fender", color:"black", string:"6 strings"}; //array with constant values
+    Musical_Instrument.color = "blue"; //change a variable in a constant array
+    Musical_Instrument.price = "£900" //add new variable
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price + ". It also has " + Musical_Instrument.string + "."; //print string on page
+}
+
+var Q = 82; //set variable
+document.write(Q);
+{
+    let Q = 33; //temporary override to X
+    document.write ("<br>" + Q);
+}
+document.write ("<br>" + Q); //X back to origional variable
+
+var Q = 72; //set Q to new variable
+document.write ("<br>" + Q);
+{
+    var Q = 113; //Override Q to new variab;e
+    document.write ("<br>" + Q);
+}
+document.write ("<br>" + Q); //Q is the latest varable
+
+document.getElementById("square_area").innerHTML = getRectArea(9, 10); // set area  width and height for function for print
+function getRectArea(width, height) { //set function
+  if (width > 0 && height > 0) {
+    return width * height; //return is a stop for a function
+  }
+  return 0;
+}
+
+let car = {
+ make: "Dodge ",
+ model:"Viper ",
+ year: "2021 ",
+ color: "red ",
+ description: function() {
+    return "The car is a " + this.year + this.color + this.make + this.model;
+ }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+
+
+function count_up() {
+let text = "";
+
+for (let i = 0; i < 10; i++) { // set i to 0 and incliment by one while i is less than 10
+    if (i === 7) {break} //break stops the function when the condition is met, in this case 7
+    text += "The number is ." + i + "<br>"; //set text that can be used even tick of the function
+}
+
+document.getElementById("count").innerHTML = text; //print text with every tick of the function 
 }
--- README.md
+++ README.md
@@ -16,3 +16,4 @@ JavaScript Fundamentals - Started
 1.2.5 Finished Project8_string_methods Assignment
 1.2.6 Finished Project_9_Countdown_slideshow Assignment
 1.2.6.5 Started Project10_loops_arrays
+1.2.7 Finished Project10_loops_arrays Assignment
